ed0c7dbd3399c11eddc6129ae823a9445c586168
Alex Koch <alex.koch@mastercard.com>
2020-04-14 14:58:31 -0500

    adds error handling for requiring libraries

--- util/main.rb
+++ util/main.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby -I~/git/chef/lib/chef/lib
 require 'chef'
-require 'chef/resource_inspector'
+# require 'chef/resource_inspector'
 require 'berkshelf'
 require 'pry'
+require 'optparse'
+
+@options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: util/main.rb [options]"
+
+end.parse!
 
 @collected_resources      = {}
 @collected_metadata_items = []
@@ -49,34 +56,89 @@ def collect_custom_resources
   cookbook_store = Berkshelf::CookbookStore.new(Berkshelf::CookbookStore.default_path)
   cached_cookbooks = {}
   # Pull out cookbook names pointing to cookbook paths
-  cookbook_store.cookbooks.each { |cb| cached_cookbooks[cb.name] = cb.path.to_s }
-  cached_cookbooks.each do |name, path|
+  cookbook_store.cookbooks.each { |cb| cached_cookbooks[cb.cookbook_name] = cb.path.to_s }
+  skipped_cookbooks = {}
+  extract_cookbooks(cached_cookbooks, skipped_cookbooks)
+  extract_cookbooks(skipped_cookbooks, skipped_cookbooks)
+end
+
+def extract_cookbooks(cookbooks, skipped_cookbooks)
+  cookbooks.each do |name, path|
     path = File.expand_path(path)
     dir = File.split(path)[0]
     loader = Chef::CookbookLoader.new(dir)
-    cookbooks = loader.load_cookbooks
+    cookbooks = Gem::Version.new(Chef::VERSION) > Gem::Version.new(14) ? loader.load_cookbooks : loader.load_cookbooks_without_shadow_warning
 
     # Get all resources from each cookbook
     resources = cookbooks[name].files_for(:resources)
     # Find all libraries and then require them so Chef can build the resources.
     libraries = cookbooks[name].files_for(:libraries)
-    libraries.each { |l| require l['full_path'] }
+    skip = false
+    libraries.each do |library| 
+      begin
+        require library['full_path']
+      rescue
+        skipped_cookbooks[name] = path
+        skip = true
+      end
+    end
+    next if skip
     resource = {}
 
     resources.each do |r|
       pth = r['full_path']
       cur = Chef::Resource::LWRPBase.build_from_file(name, pth, Chef::RunContext.new(Chef::Node.new, nil, nil))
-      resource[cur.resource_name] = ResourceInspector.extract_resource(cur, true)
+      resource[cur.resource_name] = Gem::Version.new(Chef::VERSION) > Gem::Version.new(14) ? ResourceInspector.extract_resource(cur, true) : extract_resource(cur, true)
     end
     add_resources(resource)
   end
 end
 
+def extract_resource(resource, complete = false)
+  data = {}
+  data[:description] = resource.description if resource.respond_to?(:description)
+  # data[:deprecated] = resource.deprecated || false
+  data[:default_action] = resource.default_action if resource.respond_to?(:default_action)
+  data[:actions] = resource.allowed_actions if resource.respond_to?(:allowed_actions)
+  data[:examples] = resource.examples if resource.respond_to?(:examples)
+  data[:introduced] = resource.introduced if resource.respond_to?(:introduced)
+  data[:preview] = resource.preview_resource if resource.respond_to?(:preview_resource)
+
+  properties = unless complete
+                  resource.properties.reject { |_, k| k.options[:declared_in] == Chef::Resource || k.options[:skip_docs] }
+                else
+                  resource.properties.reject { |_, k| k.options[:skip_docs] }
+                end
+
+  data[:properties] = properties.each_with_object([]) do |(n, k), acc|
+    opts = k.options
+    acc << { name: n, description: opts[:description],
+              introduced: opts[:introduced], is: opts[:is],
+              deprecated: opts[:deprecated] || false,
+              required: opts[:required] || false,
+              default: opts[:default_description] || get_default(opts[:default]),
+              name_property: opts[:name_property] || false,
+              equal_to: opts[:equal_to] || [] }
+  end
+  data
+end
+
+def get_default(default)
+    if default.is_a?(Chef::DelayedEvaluator)
+      # ideally we'd get the block we pass to `lazy`, but the best we can do
+      # is to get the source location, which then results in reparsing the source
+      # code for the resource ourselves and just no
+      "lazy default"
+    else
+      default.is_a?(Symbol) ? default.inspect : default # inspect properly returns symbols
+    end
+  end
+
 def collect_native_resources
   resource_classes = Chef::Resource.descendants
   resource_classes.each do |klass|
     resource = {}
-    resource[klass.resource_name] = ResourceInspector.extract_resource(klass, true)
+    resource[klass.resource_name] = Gem::Version.new(Chef::VERSION) > Gem::Version.new(14) ? ResourceInspector.extract_resource(klass, true) : extract_resource(klass, true)
     add_resources(resource) unless klass.resource_name.nil?
   end
 end
